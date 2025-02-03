17a009880e65e2677dfe957676a084cce8a2bdcd
Bebesi László - PC <bebesilaci@gmail.com>
2025-02-03 21:00:00 +0100

    l02 and l03 materials

---
+++ L01_intro/s01_intro.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
+    # Nem feltétlenül szükséges de hasznos
+    # Elkülönülnek a program elemek és a script-ben ténylegesen fut elemek...
