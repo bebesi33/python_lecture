920e930c180202b7a182e371ac82db86f18e78ce
Bebesi László - PC <bebesilaci@gmail.com>
2025-02-23 12:32:07 +0100

    session 2

---
+++ L02_datatypes/s02_F5_program.py
@@ -0,0 +1,8 @@
+def main():
+    first_name = input("Add meg a keresztneved!")
+    last_name = input("Add meg a vezetékneved!")
+    print(f"Hello : {last_name} {first_name}!")
+
+
+if __name__ == "__main__":
+    main()
