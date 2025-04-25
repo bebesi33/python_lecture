2f59e1087e05af69ecd0e75fcc461987f0767686
Bebesi László - PC <bebesilaci@gmail.com>
2025-04-25 09:47:53 +0200

    s10 updates

--- L10_modules_classes_paths/s10/simple_containers/stack.py
+++ L10_modules_classes_paths/s10/simple_containers/stack.py
@@ -1,1 +1,32 @@
+from .base_container import CustomContainer
+
 # TODO - ez az órai feladat
+
+
+class Stack(CustomContainer):
+    def __init__(self):
+        self._items = []
+
+    def add(self, item):
+        self._items.append(item)
+
+    def remove(self):
+        """Kivesz és visszaad egy adott elemet"""
+        if self.is_empty():
+            raise IndexError("We cannot remove from an empty Stack!")
+        return self._items.pop()
+
+    def is_empty(self):
+        return len(self._items) == 0
+
+    def size(self):
+        return len(self._items)
+
+    def __str__(self):
+        if self.is_empty():
+            return "Empty Stack"
+        else:
+            return f"""Stack({''.join([str(c) for c in self._items])})"""
+
+    def check_last_elemt(self):
+        return self._items[-1]
